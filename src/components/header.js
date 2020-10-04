05d8b9fdc4df6ac658c126ebc8a46b74ff0f119b
Simon Huang <simon.huang@outlook.de>
2020-10-04 17:21:51 +0200

    refactor Link from variable to component

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,40 @@
 import React from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-const Header = props => {
-  const link = (path, name) => (
-    <AniLink
-      fade
-      activeClassName="menu__item--active"
-      className="menu__item"
-      to={path}
-    >
-      {name}
-    </AniLink>
-  )
+const Link = props => (
+  <AniLink
+    fade
+    activeClassName="menu__item--active"
+    className="menu__item"
+    to={props.path}
+  >
+    {props.name}
+  </AniLink>
+)
 
-  return (
-    <header className="header" role="banner">
-      <input
-        type="checkbox"
-        className="menu-checkbox visually-hidden"
-        id="menu-toggle"
-      />
-      <label for="menu-toggle" className="menuicon" role="button">
-        <a className="visually-hidden">open menu</a>
-        <div className="menuicon__line menuicon__line--top"></div>
-        <div className="menuicon__line menuicon__line--middle"></div>
-        <div className="menuicon__line menuicon__line--bottom"></div>
-      </label>
+const Header = props => (
+  <header className="header" role="banner">
+    <input
+      type="checkbox"
+      className="menu-checkbox visually-hidden"
+      id="menu-toggle"
+    />
+    <label for="menu-toggle" className="menuicon" role="button">
+      <a className="visually-hidden">open menu</a>
+      <div className="menuicon__line menuicon__line--top"></div>
+      <div className="menuicon__line menuicon__line--middle"></div>
+      <div className="menuicon__line menuicon__line--bottom"></div>
+    </label>
 
-      <nav className="menu" role="navigation" aria-expanded="true">
-        {link("/", "home")}
-
-        {link("/tags", "tags")}
-
-        {link("/contact", "contact")}
-
-        <a className="menu__item" href="#" onClick={props.themeChange}>
-          theme
-        </a>
-      </nav>
-    </header>
-  )
-}
+    <nav className="menu" role="navigation" aria-expanded="true">
+      <Link path="/" name="home" />
+      <Link path="/tags" name="tags" />
+      <Link path="/contact" name="contact" />
+      <a className="menu__item" href="#" onClick={props.themeChange}>
+        theme
+      </a>
+    </nav>
+  </header>
+)
 
 export default Header
